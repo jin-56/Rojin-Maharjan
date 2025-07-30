67162e43f1d9178136877689fa55d26aebdd301f
Rojin Maharjan <137381319+jin-56@users.noreply.github.com>
2025-07-30 23:28:47 +0545

    Portfolio changes

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
